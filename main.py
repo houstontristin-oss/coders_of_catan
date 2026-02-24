0abb3f4fcadaa40537a11120e71a7fc15f4444e3
amandabarth <112357592+amandabarth@users.noreply.github.com>
2026-02-24 16:16:50 -0500

    outline for setup phase of gameplay

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
 # Runner file
+
+def setup():
+    pass
+    #NOTE: not exactly sure where to put this yet but here is the general idea
+    #loop through players
+
+    #get player selection of settlement 1
+
+    #get player selection of road 1
+
+    #loop through players in reverse
+
+    #get player selection of settlement 2
+
+    #get player selection of road 2
+
+    #give players the resources from their settlement 2
