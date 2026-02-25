ac9c17c5c95d8faf0483fd3efe6da4e97f07cf78
Apoorva Joshi <143425603+aajoshi21@users.noreply.github.com>
2026-02-25 16:44:19 -0500

    created player class file

---
+++ player.py
@@ -0,0 +1,14 @@
+class Player:
+    def __init__(self):
+        self.victory_points = 0
+        self.resource_cards  # we'll come back to this
+        self.development_cards
+
+    def accept_trade(self): #option to accept a trade from a player
+        pass
+
+    def offer_trade(self): #offer a trade to another player
+        pass
+
+    def purchase(self): #buy resources, dev cards, ports, buildings
+        pass
