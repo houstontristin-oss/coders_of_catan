0d503f9e8715996236c3e2c60f49d95b8aae1b40
amandabarth <112357592+amandabarth@users.noreply.github.com>
2026-03-02 15:48:21 -0500

    getting rid of build view

--- frontend.py
+++ frontend.py
@@ -363,9 +363,6 @@ class CatanView(arcade.View):
         if (start_x <= x <= start_x + btn_w) and (y <= HUD_BOTTOM_HEIGHT):
             self.window.show_view(TradeView(self.board, self.players, self.current_player))
 
-        if (start_x + btn_w + gap <= x <= start_x + btn_w + gap + btn_w) and (y <= HUD_BOTTOM_HEIGHT):
-            self.window.show_view(BuildView(self.board, self.players, self.current_player))
-
         if (start_x + 2*(btn_w + gap) <= x <= start_x + 2*(btn_w + gap) + btn_w) and (y <= HUD_BOTTOM_HEIGHT):
             self.window.show_view(PlayCardView(self.board, self.players, self.current_player))
 
@@ -463,42 +460,6 @@ class TradeView(arcade.View):
         if (SCREEN_WIDTH - btn_w - 20 <= x <= SCREEN_WIDTH - 20) and (y <= HUD_BOTTOM_HEIGHT):
             self.window.show_view(CatanView(self.board, self.players, self.current_player))
 
-# ---------------------------------------------------------------------------
-# Build View
-# ---------------------------------------------------------------------------
-class BuildView(arcade.View):
-    def __init__(self, board, players, current_player):
-        super().__init__()
-        self.board= board
-        self.players = players
-        self.current_player = current_player
-    
-    def _build_text_objects(self):
-        bar_center_y = HUD_BOTTOM_HEIGHT / 2
-        btn_w = 150
-        self.txt_back = arcade.Text("Back to Board",  SCREEN_WIDTH - btn_w * 0.5 - 20,    bar_center_y, TEXT_WHITE, 13, bold=True, anchor_x="center", anchor_y="center", font_name="MedievalSharp")
-
-    def _draw_bottom_bar(self):
-        fill_rect(0, 0, SCREEN_WIDTH, HUD_BOTTOM_HEIGHT, HUD_BG)
-
-        btn_w, btn_h = 150, 50
-        btn_bottom = (HUD_BOTTOM_HEIGHT - btn_h) / 2
-
-        fill_rect(SCREEN_WIDTH - btn_w - 20, btn_bottom, btn_w, btn_h, BTN_ENDTURN)
-        self.txt_back.draw()
-
-    def on_show_view(self):
-        self._build_text_objects()
-
-    def on_draw(self):
-        self.clear()
-        self._draw_bottom_bar()
-
-    def on_mouse_press(self, x, y, button, modifiers):
-        #NOTE: Would it be a good idea to make the btn_w and btn_h global variable?
-        btn_w = 150
-        if (SCREEN_WIDTH - btn_w - 20 <= x <= SCREEN_WIDTH - 20) and (y <= HUD_BOTTOM_HEIGHT):
-            self.window.show_view(CatanView(self.board, self.players, self.current_player))
 # ---------------------------------------------------------------------------
 # Play Card View
 # ---------------------------------------------------------------------------
