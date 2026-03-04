cb13f9dd10f0ddb044f3ed4a80fad99afc9951b0
Apoorva Joshi <143425603+aajoshi21@users.noreply.github.com>
2026-03-04 15:43:45 -0500

    import catanboard in frontend

--- frontend.py
+++ frontend.py
@@ -2,8 +2,8 @@ import arcade
 import math
 import os
 import pyglet
-from player import Player
-from backend import CatanBoard
+from backend.Player import Player
+from backend.CatanBoard import CatanBoard
 
 # TODO: Render 9 randomized ports around the board perimeter, visually correct, future-ready for backend trade logic.  This needs to be implemented in a way to ensure that im not stepping on the toes of those who are working on backend.py, main.py, and player.py
 #   This should safely implement our port system without overwriting other existing systems or changing how the already coded game functions.  This is just a display of the ports, which should be ready for future implementation of the port logic
