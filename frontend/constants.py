444ec1a113501528edc1f191f8b46dc1ced3c550
TremsOrbit <thetertiary606@gmail.com>
2026-03-27 11:12:53 -0400

    comments

--- frontend/constants.py
+++ frontend/constants.py
@@ -8,7 +8,7 @@ import os
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 # ---------------------------------------------------------------------------
-# Window Size
+# Window Size -
 SCREEN_WIDTH  = 1000
 SCREEN_HEIGHT = 701
 SCREEN_TITLE  = "Coders of Catan"
