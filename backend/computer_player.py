1dcda629cf5ef3db65ef13320c7a52f0db2b93ec
Apoorva Joshi <143425603+aajoshi21@users.noreply.github.com>
2026-04-03 16:04:57 -0400

    small things part 2

--- backend/computer_player.py
+++ backend/computer_player.py
@@ -1,5 +1,7 @@
 # pylint: disable = R0902
+
 from .player import Player
+
 class ComputerPlayer(Player):
     """Represents a computer player for Catan
 
@@ -10,39 +12,41 @@ class ComputerPlayer(Player):
         all of Player's attributes
         computer: returns bool
     """
-    """to add to computer player class to make this easier
-            - can_place_settlement() : returns node to place settlement at or None
-            - best_road_location() : returns edge to place road on
-            - best_city_location() : returns node to make into a city
-            - play_dev_card() : returns str of what dev card was played for log
-    """
+
     def __init__(self, color, name):
         super().__init__(color, name)
         self.computer = True
-
 
     def can_afford_road(self):
         return bool(self.resource_cards['BRICK'] > 0 and self.resource_cards['WOOD'] > 0)
 
     def can_afford_settlement(self):
         return bool(self.resource_cards['BRICK'] > 0 and self.resource_cards['WOOD'] > 0 and
-                self.resource_cards['WHEAT'] > 0 and self.resource_cards['SHEEP'] > 0)
+                    self.resource_cards['WHEAT'] > 0 and self.resource_cards['SHEEP'] > 0)
 
     def can_afford_city(self):
         return bool (self.resource_cards['WHEAT'] >= 2 and self.resource_cards['ORE'] >= 3)
 
     def can_afford_dev_card(self):
         return bool(self.resource_cards['WHEAT'] >= 0 and self.resource_cards['ORE'] >= 0 and
-                self.resource_cards['SHEEP'] > 0)
+                    self.resource_cards['SHEEP'] > 0)
 
-    def best_city_location(self):
-        pass
-
+    # returns edge to place road at or None
     def best_road_location(self):
-        pass
+        if self.can_afford_road():
+            pass
 
-    def can_place_settlement(self):
-        pass
+    # returns node to place settlement at or None
+    def best_settlement_location(self):
+        if self.can_afford_settlement():
+            pass
 
+    # returns node to place city at or None
+    def best_city_location(self):
+        if self.can_afford_city():
+            pass
+
+    # returns string of what dev card was played for log
     def play_dev_card(self):
-        pass
+        if self.can_afford_dev_card():
+            pass
