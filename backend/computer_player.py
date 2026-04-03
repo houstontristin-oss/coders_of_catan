cdfb619f382cebd536309d4b96c3589e4592f73d
Apoorva Joshi <143425603+aajoshi21@users.noreply.github.com>
2026-04-03 15:54:19 -0400

    pylinted "afford" methods

--- backend/computer_player.py
+++ backend/computer_player.py
@@ -1,4 +1,4 @@
-# pylint: disable=C0114,C0116, R0902
+# pylint: disable = R0902
 from .player import Player
 class ComputerPlayer(Player):
     """Represents a computer player for Catan
@@ -12,12 +12,8 @@ class ComputerPlayer(Player):
     """
     """to add to computer player class to make this easier
             - can_place_settlement() : returns node to place settlement at or None
-            - can_afford_road() : returns bool
             - best_road_location() : returns edge to place road on
-            - can_afford_settlement() : returns bool
-            - can_afford_city() : returns bool
             - best_city_location() : returns node to make into a city
-            - can_afford_dev_card() : returns bool
             - play_dev_card() : returns str of what dev card was played for log
             - 
     """
@@ -27,30 +23,18 @@ class ComputerPlayer(Player):
 
 
     def can_afford_road(self):
-        if self.resource_cards['BRICK'] > 0 and self.resource_cards['WOOD'] > 0:
-            return True
-        else:
-            return False
+        return bool(self.resource_cards['BRICK'] > 0 and self.resource_cards['WOOD'] > 0)
 
     def can_afford_settlement(self):
-        if (self.resource_cards['BRICK'] > 0 and self.resource_cards['WOOD'] > 0 and
-                self.resource_cards['WHEAT'] > 0 and self.resource_cards['SHEEP'] > 0):
-            return True
-        else:
-            return False
+        return bool(self.resource_cards['BRICK'] > 0 and self.resource_cards['WOOD'] > 0 and
+                self.resource_cards['WHEAT'] > 0 and self.resource_cards['SHEEP'] > 0)
 
     def can_afford_city(self):
-        if self.resource_cards['WHEAT'] >= 2 and self.resource_cards['ORE'] >= 3:
-            return True
-        else:
-            return False
+        return bool (self.resource_cards['WHEAT'] >= 2 and self.resource_cards['ORE'] >= 3)
 
     def can_afford_dev_card(self):
-        if (self.resource_cards['WHEAT'] >= 0 and self.resource_cards['ORE'] >= 0 and
-                self.resource_cards['SHEEP'] > 0):
-            return True
-        else:
-            return False
+        return bool(self.resource_cards['WHEAT'] >= 0 and self.resource_cards['ORE'] >= 0 and
+                self.resource_cards['SHEEP'] > 0)
 
     def best_city_location(self):
         pass
@@ -60,7 +44,3 @@ class ComputerPlayer(Player):
 
     def can_place_settlement(self):
         pass
-
-
-
-
